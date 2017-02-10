0426200000d59e57625e652115a3ec18e1a42ec7
Ewald Stieger <e.stieger@gmail.com>
2017-02-10 13:37:30 +0200

    Add token filter attribute for Swagger

---
+++ WebApiSecure/Filters/RequireTokenAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+
+namespace WebApiSecure.Filters
+{
+    //Only a tagging filter for Swagger integration
+    public class RequireTokenAttribute: ActionFilterAttribute
+    {
+        
+    }
+}
